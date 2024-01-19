fde188860f4059fcbc50e3f4b8f7023d85877d94
Nathan Perez <naperezmail@gmail.com>
2024-01-18 19:38:54 -0500

    added to addBooks method

--- SDevLMS.java
+++ SDevLMS.java
@@ -6,39 +6,20 @@ import java.util.Scanner; //import scanner class
 
 public class SDevLMS
 {
-    void AddBook(int id, String title, String author)
-    {
-
-    }
-    void GetUserBooks()
-    {
-
-    }
-
-    void GetUserBook()
-    {
-
-    }
-
-    void RemoveBook()
-    {
-
-    }
-
-
-
-    void
 
     public static void main(String[] args)
     {
         Scanner scan = new Scanner(System.in);
+
+        //create Library (array of books)
+        Book[] library = new Book[100];
 
         System.out.println("Welcome to the Library Management Software!");
         //while loop for select and performing functions
         while(true)
         {
             //user options
-            System.out.println("Please select an option from the ones below by entering the corresponding number.");
+            System.out.println("\nPlease select an option from the ones below by entering the corresponding number.");
             System.out.println("1 - Add books from a text file");
             System.out.println("2 - Add a new book");
             System.out.println("3 - Remove a book");
@@ -68,7 +49,7 @@ public class SDevLMS
             else if(choice == 4) //print
             {
                 //print book function
-                System.out.println("this is a test");
+                PrintBooks(library);
             }
             else
             {
@@ -78,4 +59,38 @@ public class SDevLMS
         } //end while loop for running program
 
     } //end main
+
+    private static void AddBook(int id, String title, String author)
+    {
+
+    }
+    private static void GetUserBooks()
+    {
+
+    }
+
+    private static void GetUserBook()
+    {
+
+    }
+
+    private static void RemoveBook()
+    {
+
+    }
+
+    //prints all books in the library
+    private static void PrintBooks(Book[] library)
+    {
+        if(Book.numBooks > 0) {
+            System.out.println("Books: ");
+            //for each book in the library
+            for (int i = 0; i < Book.numBooks; i++) {
+                library[i].Print();
+            }
+        }
+        else
+            System.out.println("You currently do not have any books. ");
+    }
+
 } //end public class SDevLMS
