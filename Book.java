1ccc77a717803b1d7dcefd72efa0733a7eb7ec1d
Nathan Perez <naperezmail@gmail.com>
2024-02-29 12:43:39 -0500

    Changed id to barcode, added search using title and barcode methods, added checkin and checkout methods to book class, added CheckInBook and CheckOutBook to SDevLMS, edited main to handle new functions.

--- Book.java
+++ Book.java
@@ -5,17 +5,34 @@
 //Book Class
 //stores  title, author, barcode number, genre, status, and due date
 
+import java.time.LocalDate;
+
 public class Book {
     static int numBooks = 0;
-    int id = -1;
+    int barcode = -1;
     String title;
     String author;
     String genre;
-    boolean status; 
-    //date dueDate;
+    boolean status;
+    LocalDate dueDate = null;
 
     //prints the book with commas separating attributes (no spaces).
     void Print() {
-        System.out.print(id + "," + title + "," + author);
+        System.out.print(barcode + "," + title + "," + author + "," + genre + "," + status + "," + dueDate);
+    }
+
+    //sets the due date to the date in 4 weeks.
+    void Checkout() {
+        status = false;
+        dueDate = LocalDate.now().plusWeeks(4);
+        Print();
+        System.out.println(" has been checked out!");
+    }
+
+    void Checkin() {
+        status = true;
+        dueDate = null;
+        Print();
+        System.out.println(" has been checked in!");
     }
 }
--- SDevLMS.java
+++ SDevLMS.java
@@ -4,8 +4,10 @@
 //Last edited 25 Jan 2024
 
 import java.io.*;
+import java.util.Objects;
 import java.util.Scanner; //import scanner class
 import java.io.FileReader; //import file reader class
+import java.time.LocalDate; //import time class
 
 public class SDevLMS
 {
@@ -25,18 +27,20 @@ public class SDevLMS
             library[i] = new Book();
         }
 
-        System.out.println("Welcome to the Library Management Software!");
+        System.out.println("\nWelcome to the Library Management Software!");
 
         //while loop for select and performing functions
         while(true)
         {
             //user options
             System.out.println("\nPlease select an option from the ones below by entering the corresponding number.");
-            System.out.println("1 - Add books from a text file");
-            System.out.println("2 - Add a new book");
-            System.out.println("3 - Remove a book");
-            System.out.println("4 - Print library");
-            System.out.println("-1 - Quit");
+            System.out.println("1  -  Add books from a text file");
+            System.out.println("2  -  Add a new book");
+            System.out.println("3  -  Remove a book");
+            System.out.println("4  -  Check-out a book");
+            System.out.println("5  -  Check-in a book");
+            System.out.println("6  -  Print library");
+            System.out.println("-1 -  Quit");
 
             //create integer that stores users option choice
             int choice = 0;
@@ -74,7 +78,17 @@ public class SDevLMS
                 //remove book method
                 RemoveBook(library);
             }
-            else if(choice == 4) //print
+            else if(choice == 4) //check out
+            {
+                //check out method
+                CheckOutBook(library);
+            }
+            else if(choice == 5) //check in
+            {
+                //check in method
+                CheckInBook(library);
+            }
+            else if(choice == 6) //print
             {
                 //print book method
                 PrintBooks(library);
@@ -90,18 +104,22 @@ public class SDevLMS
 
     //----------------------- METHODS -------------------------------------------------------------------
 
-    //checks availability of ID in library
-    //takes in the ID and library
-    //Returns true if ID is available, and false if ID has already been used
+
+    // CHECK ID AVAILABILITY
+    /*checks availability of ID in library
+    takes in the ID and library
+    Returns true if ID is available, and false if ID has already been used */
     private static boolean IdAvailability(int id, Book[] library)
     {
         for (Book book : library) {
-            if (book.id == id)
+            if (book.barcode == id)
                 return false;
         }
         return true;
     }
 
+
+    // CHECK IF THERE ARE BOOKS IN LIBRARY
     //checks to see if there are any books in the library array
     //Takes in the library
     //If there are books, return true, else return false
@@ -109,7 +127,7 @@ public class SDevLMS
     {
         for (Book book : library) {
             //if there are any books return true
-            if (book.id != -1)
+            if (book.barcode != -1)
                 return true;
         }
         //if it gets to this point there are no books in the array
@@ -117,9 +135,34 @@ public class SDevLMS
     }
 
 
+    // FIND BOOK USING BARCODE
+    //finds a book in the library using the barcode, returns location of book in array, or -1 if book can't be found.
+    private static int FindBookUsingBarcode(int barcode, Book[] library)
+    {
+        for (int i = 0; i < library.length; i++) {
+            if (library[i].barcode == barcode)
+                return i;
+        }
+        return -1; //book could not be found
+    }
+
+
+    // FIND BOOK USING TITLE
+    //finds a book in the library using the title, returns location of book in array, or -1 if book can't be found.
+    private static int FindBookUsingTitle(String title, Book[] library)
+    {
+        for (int i = 0; i < library.length; i++) {
+            if (Objects.equals(library[i].title, title))
+                return i;
+        }
+        return -1; //book could not be found
+    }
+
+
+    // ADD BOOK
     //Adds the book to the first opening in the library
     //Takes in the ID, Title, and Author of book that is being created. Also takes in library
-    private static void AddBook(int id, String title, String author, Book[] library)
+    private static void AddBook(int id, String title, String author, String genre, boolean status, LocalDate dueDate, Book[] library)
     {
         //if ID is available
         if(IdAvailability(id, library))
@@ -131,16 +174,20 @@ public class SDevLMS
             //for loop that goes through library until it gets to an opening
             //checks for opening at the beginning first
             for (int i = 0; i < (Book.numBooks); i++) {
-                if (library[i].id == -1) {
+                if (library[i].barcode == -1) {
                     openSpace = i;
                     break;
                 }
             }
 
             //add book to opening in array
-            library[openSpace].id = id;
+            library[openSpace].barcode = id;
             library[openSpace].title = title;
             library[openSpace].author = author;
+            library[openSpace].genre = genre;
+            library[openSpace].status = status;
+            library[openSpace].dueDate = dueDate;
+
 
             //print book for the user to let them know it has been added
             library[openSpace].Print();
@@ -150,36 +197,50 @@ public class SDevLMS
         else System.out.println("Error, that ID has already been used.");
     }
 
+
+    // GET A BOOK FROM A LINE
     //Separates line by commas and stores in id, title and author with their respective data types.
     //Sends this data to the AddBook method
     //Takes in a string (a line from the file), and library
     private static void GetBookFromLine(String line, Book[] library)
     {
         //declare variables that will hold the id, title, and author
-        int id;
+        int barcode;
         String title;
         String author;
+        String genre;
+        boolean status;
+        LocalDate dueDate;
+
 
         //split line into 3 strings (stored in array stringBook) using commas.
-        String[] stringBook = line.split(",", 3);
+        String[] stringBook = line.split(",", 6);
 
         //set values of id, title, and author to values found in line
-        id = Integer.parseInt(stringBook[0]); //change id from string to int
+        barcode = Integer.parseInt(stringBook[0]); //change barcode from string to int
         title = stringBook[1];
         author = stringBook[2];
+        genre = stringBook[3];
+        status = Boolean.parseBoolean(stringBook[4]);
+        dueDate = LocalDate.parse(stringBook[5]);
 
         //add book to the library
-        AddBook(id, title, author, library);
+        AddBook(barcode, title, author, genre, status, dueDate, library);
     }
 
 
+    // READ FILE
     //Opens file and sends stores each line in a string called line.
     //This is then sent to the GetBookFromLine method
     //Takes in library, does not return anything.
     private static void ReadFile(Book[] library) throws IOException {
 
+        String fileName;
+        System.out.print("Please enter the name of the text file you would like to use. (Do not include .txt in your input");
+        fileName = scan.nextLine();
+
         //open file
-        BufferedReader reader = new BufferedReader(new FileReader("libraryBooks.txt"));
+        BufferedReader reader = new BufferedReader(new FileReader(fileName + ".txt"));
 
         String line;
         while((line = reader.readLine()) != null)
@@ -189,33 +250,37 @@ public class SDevLMS
     }
 
 
-    //get id, title, and author from user and run AddBook method
+    // GET A BOOK FROM THE USER
+    //get barcode, title, and author from user and run AddBook method
     //takes in library, returns nothing
     private static void GetUserBook(Book[] library)
     {
         //declare variables
-        int id = -1;
+        int barcode = -1;
         String title;
         String author;
-        //String removeNewLine;
+        String genre;
+        boolean status;
+        LocalDate dueDate;
 
-        //Prompt user to enter id, title, and author.
+
+        //Prompt user to enter barcode, title, and author.
         System.out.println("Adding a book...");
-        System.out.println("Please enter the ID, Title, and Author");
+        System.out.println("Please enter the Barcode Number, Title, Author, and Genre");
 
         //make sure that ID is available and allow user to try a different ID if it is not.
         boolean available = false;
 
         while(!available) {
-            //get id from user
+            //get barcode from user
             System.out.print("ID: ");
-            id = scan.nextInt();
+            barcode = scan.nextInt();
             scan.nextLine(); //removes new line character so that title does not scan it.
 
-            available = IdAvailability(id, library);
+            available = IdAvailability(barcode, library);
 
             if(!available)
-                System.out.println("Sorry that ID has already been used. Please try again with a different ID.");
+                System.out.println("Sorry that barcode number has already been used. Please try again with a different barcode number.");
         }
 
         //get title from user
@@ -226,12 +291,23 @@ public class SDevLMS
         System.out.print("Author: ");
         author = scan.nextLine();
 
+        //get genre from user
+        System.out.print("Genre: ");
+        genre = scan.nextLine();
+
+        //automatically set all new books added by the user to checked in (true)
+        status = true;
+
+        //since the books are checked in they don't have a due date (dueDate == null)
+        dueDate = null;
+
         //add book using the AddBook method
-        AddBook(id, title, author, library);
+        AddBook(barcode, title, author, genre, status, dueDate, library);
     }
 
 
-    //removes a book from the library
+    // REMOVE BOOK
+    //removes a book from the library using either barcode or title
     //takes in library, returns nothing
     private static void RemoveBook(Book[] library)
     {
@@ -239,27 +315,49 @@ public class SDevLMS
         //if library has any books in it
         if(HasBooks(library)) //remove a book
         {
-            int id;
+            int barcode;
             int location = -1;
 
             do
-            {            //ask user for ID of book to remove
-                System.out.println("Please enter the ID of the book you would like to remove: ");
-                id = scan.nextInt();
+            {
+                //initialize choice variable. Will be changed to 1 for barcode or 2 for title.
+                int choice =-1;
 
-                //make sure that the id exists
+                do {
+                    //ask user if they want to use barcode or title to remove
+                    System.out.println("Would you like to remove the book using its barcode or title?");
+                    System.out.println("1 - Barcode");
+                    System.out.println("2 - Title");
+                    choice = scan.nextInt();
 
-                //find the location of the book in the library
-                for (int i = 0; i < library.length; i++) {
-                    if (id == library[i].id)
-                        location = i;
-                }
+                    if (choice == 1) //user chose 1, remove by barcode
+                    {
+                        //ask user for barcode of book to remove
+                        System.out.println("Please enter the barcode number of the book you would like to remove: ");
+                        barcode = scan.nextInt();
 
-                //if location is still -1 book with that id must not exist
+                        //find the location of the book in the library
+                        location = FindBookUsingBarcode(barcode, library);
+                    } else if (choice == 2) //user chose 2, remove by title
+                    {
+                        //ask user for title of book to remove
+                        System.out.println("Please enter the title of the book you would like to remove: ");
+                        scan.next(); //remove new line char
+                        String title = scan.next();
+
+                        //find the location of the book in the library
+                        location = FindBookUsingTitle(title, library);
+                    } else //user didn't choose 1 or 2.
+                    {
+                        System.out.println(choice + " is not a valid option. Please try again.");
+                    }
+                } while (choice != 1 && choice != 2); //repeat if user enters an invalid option
+
+                //if location is still -1 book with that barcode must not exist
                 if(location == -1)
-                    System.out.println("Error, no book matches the ID you entered.");
+                    System.out.println("Error, no book could be found.");
 
-            } while (location == -1);
+            } while (location == -1); //repeat if book can not be found. Allow user to retry
 
             //check whether user would like to remove the given book.
             int yn; //stores 1 for yes and 2 for no
@@ -269,14 +367,14 @@ public class SDevLMS
                 library[location].Print();
                 System.out.println("?");
 
-                System.out.println("1. Yes");
-                System.out.println("2. No");
+                System.out.println("1 - Yes");
+                System.out.println("2 - No");
                 yn = scan.nextInt();
 
                 if (yn == 1) {
                     //remove book using its location
-                    //set id to -1 so that it can be overwritten
-                    library[location].id = -1;
+                    //set barcode to -1 so that it can be overwritten
+                    library[location].barcode = -1;
                 }
                 else if (yn == 2) {
                     //don't remove book
@@ -293,18 +391,70 @@ public class SDevLMS
     }
 
 
+    // CHECK OUT BOOK
+    //takes in library, returns nothing
+    private static void CheckOutBook(Book[] library)
+    {
+        String title;
+        int location;
+
+        do {
+            //get title from the user
+            System.out.print("Please enter the title of the book you would like to check out: ");
+            title = scan.next();
+
+            //get location in library using the title
+            location = FindBookUsingTitle(title, library);
+
+            if(location != -1)
+            {
+                //checkout book using book's method 'Checkout()' .
+                library[location].Checkout();
+            }
+            else
+                System.out.println("Error, could not find the book " + title + ". Please try again.");
+        } while (location == -1);
+    }
+
+
+    // CHECK IN BOOK
+    //takes in library, returns nothing
+    private static void CheckInBook(Book[] library)
+    {
+        String title;
+        int location;
+
+        do {
+            //get title from the user
+            System.out.print("Please enter the title of the book you would like to check in: ");
+            title = scan.next();
+
+            //get location in library using the title
+            location = FindBookUsingTitle(title, library);
+
+            if(location != -1)
+            {
+                //check in book using book's method 'Checkin()' .
+                library[location].Checkin();
+            }
+            else
+                System.out.println("Error, could not find the book " + title + ". Please try again.");
+        } while (location == -1);
+    }
+
+
+    // PRINT ALL BOOKS
     //prints all books in the library
     //takes in library, returns nothing
     private static void PrintBooks(Book[] library)
     {
-        //check to see if there are any books in library
         //check to make sure library has books in it
         if(HasBooks(library))
         {
             System.out.println("Books: ");
             //for each book in the library
             for (Book book : library) {
-                if (book.id != -1) {
+                if (book.barcode != -1) {
                     book.Print();
                     System.out.print("\n");
                 }
